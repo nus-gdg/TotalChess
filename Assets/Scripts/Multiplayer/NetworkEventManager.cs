3f0555cc05b5e31d13de40121b6f93b2c981cdd7
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2019-03-26 12:33:42 +0800

    Add sending turns and receiving turn log * FE can hook on to events * Events sent over photon renamed to NEvent * Added MPTester and migrated dummy data from StateManager

--- Assets/Scripts/Multiplayer/NetworkEventManager.cs
+++ Assets/Scripts/Multiplayer/NetworkEventManager.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,10 +9,17 @@ using Photon.Pun;
 using Photon.Realtime;
 using ExitGames.Client.Photon;
 
+using State;
+using Log;
 
 public class NetworkEventManager : MonoBehaviourPunCallbacks, IOnEventCallback
 {
-    // Start is called before the first frame update
+
+    public bool IsMasterClient()
+    {
+        return PhotonNetwork.IsMasterClient;
+    }
+
     public override void OnLeftRoom()
     {
         SceneManager.LoadScene(0);
@@ -22,7 +30,7 @@ public class NetworkEventManager : MonoBehaviourPunCallbacks, IOnEventCallback
         PhotonNetwork.LeaveRoom();
     }
 
-    public override void OnPlayerEnteredRoom(Player other)
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player other)
     {
         Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
 
@@ -33,7 +41,7 @@ public class NetworkEventManager : MonoBehaviourPunCallbacks, IOnEventCallback
         }
     }
 
-    public override void OnPlayerLeftRoom(Player other)
+    public override void OnPlayerLeftRoom(Photon.Realtime.Player other)
     {
         Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
 
@@ -43,20 +51,97 @@ public class NetworkEventManager : MonoBehaviourPunCallbacks, IOnEventCallback
         }
     }
 
+    private byte[] serializeObject(object obj)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        MemoryStream ms = new MemoryStream();
+        bf.Serialize(ms, obj);
+        byte[] data = ms.ToArray();
+        ms.Close();
+        return data;
+    }
+
+    private object deserializeData(byte[] data)
+    {
+        MemoryStream memStream = new MemoryStream();
+        BinaryFormatter binForm = new BinaryFormatter();
+        memStream.Write(data, 0, data.Length);
+        memStream.Seek(0, SeekOrigin.Begin);
+        object obj = binForm.Deserialize(memStream);
+        memStream.Close();
+        return obj;
+    }
+
+    // FE can use this to send their move information
+    // note: Currently set as List<Move[]>
+    public void SendTurn(List<Move[]> turnList)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            // cache it here or send to game manager
+            OnReceiveTurn?.Invoke(turnList.ToArray());
+            return;
+        }
+        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.Others };
+        SendOptions sendOptions = new SendOptions { Reliability = true };
+        byte[] data = serializeObject(turnList.ToArray());
+        PhotonNetwork.RaiseEvent(SEND_TURN_NEVENT, data, raiseEventOptions, sendOptions);
+    }
+
+    public void SendTurnLog(TurnLog turnLog)
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            return; // only master can send turn logs
+        }
+        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All };
+        SendOptions sendOptions = new SendOptions { Reliability = true };
+        byte[] data = serializeObject(turnLog);
+        PhotonNetwork.RaiseEvent(SEND_TURN_LOG_NEVENT, data, raiseEventOptions, sendOptions);
+    }
+
     public void RaiseStartEvent()
     {
-        byte evCode = 0; // Custom Event 0: Used as game start
-        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All }; // You would have to set the Receivers to All in order to receive this event on the local client as well
+        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All };
         SendOptions sendOptions = new SendOptions { Reliability = true };
-        PhotonNetwork.RaiseEvent(evCode, null, raiseEventOptions, sendOptions);
+        PhotonNetwork.RaiseEvent(GAME_START_NEVENT, null, raiseEventOptions, sendOptions);
     }
+
+    // NEVENT (FE should not have to worry about this)
+    // note: NEVENT = NETWORK EVENT, need to separate from Events used by client
+    public const byte GAME_START_NEVENT = 0;
+    public const byte SEND_TURN_NEVENT = 1;
+    public const byte SEND_TURN_LOG_NEVENT = 2;
+
+    //EVENTS (FE can use these)
+    public delegate void GameStartEvent();
+    public static event GameStartEvent OnGameStart;
+
+    public delegate void ReceiveTurnEvent(Move[][] turn);
+    public static event ReceiveTurnEvent OnReceiveTurn;
+
+    // FE can hook onto this
+    public delegate void ReceiveTurnLogEvent(TurnLog turn);
+    public static event ReceiveTurnLogEvent OnReceiveTurnLog;
 
     public void OnEvent(EventData photonEvent)
     {
         byte eventCode = photonEvent.Code;
-        if (eventCode == 0)
+        switch (eventCode)
         {
-            GetComponent<Renderer>().material.color = new Color(1.0f, 0, 0);
+            case GAME_START_NEVENT:
+                OnGameStart?.Invoke();
+                break;
+            case SEND_TURN_NEVENT:
+                byte[] data = (byte[])photonEvent.CustomData;
+                Move[][] turn = (Move[][])deserializeData(data);
+                OnReceiveTurn?.Invoke(turn);
+                break;
+            case SEND_TURN_LOG_NEVENT:
+                byte[] turnData = (byte[])photonEvent.CustomData;
+                TurnLog turnLog = (TurnLog)deserializeData(turnData);
+                OnReceiveTurnLog?.Invoke(turnLog);
+                break;
         }
     }
 
